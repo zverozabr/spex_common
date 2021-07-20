bd0579ff1a3af93cfcd1fea35c01e484c5535d80
Artem Zubkov <artzub@gmail.com>
2021-07-20 15:46:10 +0700

    added in config working_dir

--- spex_common/config.py
+++ spex_common/config.py
@@ -1,4 +1,4 @@
-from os import path, environ
+from os import path, environ, getcwd
 from dotenv import dotenv_values
 
 trues = ['true', 'yes']
@@ -6,13 +6,13 @@ falses = ['false', 'no']
 int_keys = ['MAX_CONTENT_LENGTH']
 
 
-def load_config(mode='', update_environ=True):
+def load_config(mode='', update_environ=True, working_dir=getcwd()):
     mode = environ.get('MODE', mode)
 
     file = f'.{mode}' if mode else ''
 
     file = path.join(
-        path.dirname(__file__),
+        working_dir if working_dir is not None else path.dirname(__file__),
         f'.env{file}'
     )
     local = f'{file}.local'
