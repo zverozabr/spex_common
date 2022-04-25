0a72f23a890ee76225934a875ef28fe741b21a90
Artem Zubkov <zubkov.artem@gene.com>
2022-04-25 09:57:13 +0700

    templates fixed

--- spex_common/models/Templates.py
+++ spex_common/models/Templates.py
@@ -2,17 +2,17 @@ class Template:
     def __init__(self, **kwargs):
         self.id = kwargs.get('_key', '')
         self.author = kwargs.get('author', '')
-        self.date = kwargs.get('date', '')
-        self.content = kwargs.get('content', '')
-        self.parent = kwargs.get('parent', '')
+        self.data = kwargs.get('data', '')
+        self.pipeline_source = kwargs.get('pipeline_source', '')
+        self.name = kwargs.get('name', '')
 
     def to_json(self):
         return {
             'id': self.id,
             'author': self.author,
-            'date': self.date,
-            'content': self.content,
-            'parent': self.parent,
+            'data': self.data,
+            'pipeline_source': self.pipeline_source,
+            'name': self.name,
         }
 
 
